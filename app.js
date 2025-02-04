93dcd730fb2b7e7593fbe7c0a81404cc45cb0cb5
Wanley Rooby Alexis <alexroob96@gmail.com>
2025-02-04 13:04:19 -0300

    Método para adicionar amigo e limpar campo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = []; 
+
+function adicionarAmigo() {
+    let valorDoCampo = document.querySelector("input").value;
+    if (valorDoCampo == "") {
+        alert("Por favor, insira um nome.");
+    }
+    else {
+        nomes.push(valorDoCampo);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    document.querySelector("input").value = "";
+}
